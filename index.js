76886d5c8bd28e9b2256172178619efdc9fe32de
glorfin1337 <glorfin1337@gmail.com>
2021-03-20 22:32:36 +0300

    Rename schema to table, log every query to console

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const MySQL = require('mysql')
-let isDbSetup = false, con
+let isDbSetup = false, con, logCount = 0
 
 module.exports = function() {
 
@@ -10,24 +10,34 @@ module.exports = function() {
             password: params.password,
             database: params.database
         }), isDbSetup = true
+
+        con.connect((err) => {
+            if (err) throw err
+            logCount++
+            console.log(`[GLORM LOG: #${logCount}] >> Connected to database successfully.`)
+        })
     }
     
-    this.createSchema = (name, data) => {
+    this.createTable = (name, data) => {
 
         if(!isDbSetup) throw "Database settings are not set up. Please use connectDatabase function."
 
-        var keys = Object.keys(data), values = Object.values(data), sql = `CREATE TABLE ${name} (`, query = ""
+        var keys = Object.keys(data), values = Object.values(data), sql = `CREATE TABLE IF NOT EXISTS ${name} (`, query = ""
 
         for(let i = 0; i < keys.length; i++) {
-            if(values[i].startsWith("int")) sql += `${keys[i]} INT(${values[i].replace("int - ", "")}),`
+            if(values[i].startsWith("ai")) sql += `${keys[i]} INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(${keys[i]}),`
+            else if(values[i].startsWith("int")) sql += `${keys[i]} INT(${values[i].replace("int - ", "")}),`
             else if(values[i].startsWith("char")) sql += `${keys[i]} VARCHAR(${values[i].replace("char - ", "")}),`
             else if(values[i].startsWith("float")) sql += `${keys[i]} FLOAT(${values[i].replace("float - ", "")}),`
             else if(values[i].startsWith("text")) sql += `${keys[i]} TEXT(${values[i].replace("text - ", "")}),`
-            else if(values[i].startsWith("ai")) sql += `${keys[i]} INT NOT NULL AUTO_INCREMENT,`
         }
 
         query = `${sql.slice(0, -1)})`
-        con.query(query, (err, result) => { if (err) throw err })
+        con.query(query, (err, result) => { 
+            if (err) throw err
+            logCount++
+            console.log(`[GLORM LOG: #${logCount}] >> query '${query}' has been executed.`)
+        })
     
     }
 
