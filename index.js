892b555f7defde37915af5dd2052035db3b5bd41
glorfin1337 <glorfin1337@gmail.com>
2021-03-20 19:55:35 +0300

    Basic functions such as connectDatabase and createSchema

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-class GlormJS
-{
+const MySQL = require('mysql')
+let isDbSetup = false, con
+
+module.exports = function() {
+
+    this.connectDatabase = (params) => {
+        con = MySQL.createConnection({
+            host: params.host,
+            user: params.user,
+            password: params.password,
+            database: params.database
+        }), isDbSetup = true
+    }
     
+    this.createSchema = (name, data) => {
+
+        if(!isDbSetup) throw "Database settings are not set up. Please use connectDatabase function."
+
+        var keys = Object.keys(data), values = Object.values(data), sql = `CREATE TABLE ${name} (`, query = ""
+
+        for(let i = 0; i < keys.length; i++) {
+            if(values[i].startsWith("int")) sql += `${keys[i]} INT(${values[i].replace("int - ", "")}),`
+            else if(values[i].startsWith("char")) sql += `${keys[i]} VARCHAR(${values[i].replace("char - ", "")}),`
+            else if(values[i].startsWith("float")) sql += `${keys[i]} FLOAT(${values[i].replace("float - ", "")}),`
+            else if(values[i].startsWith("text")) sql += `${keys[i]} TEXT(${values[i].replace("text - ", "")}),`
+            else if(values[i].startsWith("ai")) sql += `${keys[i]} INT NOT NULL AUTO_INCREMENT,`
+        }
+
+        query = `${sql.slice(0, -1)})`
+        con.query(query, (err, result) => { if (err) throw err })
+    
+    }
+
 }
